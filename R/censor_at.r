cf87679b896156c09275ba7f03a2223522ad1350
Luca Braglia <lbraglia@gmail.com>
2015-05-22 12:49:20 +0200

    fup_completeness and improving censor_at

--- R/censor_at.r
+++ R/censor_at.r
@@ -1,7 +1,24 @@
-#' Censor time to event end point at a certain time.
+#' Censor time to event end point at a given time/period of follow up.
 #' 
-#' Censor time to event end point at a certain time. Implemented only for
-#' "right" type of censored data.
+#' Censor time to event end point at a given time. Implemented for
+#' right-censored dataset.
+#'
+#' The algorithm works as follow.
+#' If time of censoring is higher than (or equal to) patient's time, does
+#' nothing and returns time and status as they are.
+#' Otherwise:
+#' 
+#' - if the patients didn't experienced the event at full follow up,
+#'   he/she didn't experienced even at the previous censoring time:
+#'   therefore the algorithm should set censored time but leave indicator
+#'   variable unchanged
+#' - if the patients experienced the event at time t, we hyphotize he/she
+#'   was without event at time t-1 (aka an event is experienced the same
+#'   day which is registered in the dataset), therefore the algorithm set
+#'   0 to indicator variable and set time to censoring time
+#' 
+#' ... synthesizing, in both cases set indicator variable to 0 and time
+#' to censoring time.
 #' 
 #' @param time time variable
 #' @param status status variable
@@ -9,45 +26,44 @@
 #' @return A data frame to be used with \code{\link{cbind}}
 #' @examples
 #'
+#' ctimes <- c(160, 150, 125, 75)
+#'
+#' ## Example 1:
+
 #' time   <- c(100,150)
 #' status <- c(  0,  1)
-#' (original <- data.frame(time, status))
-#' censor_at(time = time, status = status, censoring_time = 160)
-#' censor_at(time = time, status = status, censoring_time = 150)
-#' censor_at(time = time, status = status, censoring_time = 125)
-#' censor_at(time = time, status = status, censoring_time = 75)
-#' 
+#' cbind(data.frame(time, status), # original
+#'       censor_at(time = time, status = status, censoring_time = ctimes))
+#'
+#' ## Example 2
 #' time   <- c(100,150)
 #' status <- c(  1,  0)
-#' (original <- data.frame(time, status))
-#' censor_at(time = time, status = status, censoring_time = 160)
-#' censor_at(time = time, status = status, censoring_time = 150)
-#' censor_at(time = time, status = status, censoring_time = 125)
-#' censor_at(time = time, status = status, censoring_time = 75)
+#' cbind(data.frame(time, status), # original
+#'       censor_at(time = time, status = status, censoring_time = ctimes))
 #' 
 #' @export
 censor_at <- function(time = NULL, status = NULL, censoring_time = NULL) {
 
-  if (sum(sapply(list(time, status, censoring_time), is.null))>0) {
-    stop("time, status and censoring_time are mandatory.")
-  }
+  ## check input
+  if (! is.numeric(time))
+    stop("time is mandatory and must be numeric.")
+  if (! is.numeric(status))
+    stop("status is mandatory and must be numeric.")
+  if (! is.numeric(censoring_time))
+    stop("censoring_time is mandatory and must be numeric.")
+  if( length(status) != length(time))
+    stop("time and status must have the same length.")
 
-  ## Se il  tempo a cui si vuol tagliare è maggiore del tempo rilevato, 
-  ## non fare nulla e restituisci la coppia time status così come sono
+  ## normalize input
+  censoring_time <- sort(censoring_time[!is.na(censoring_time)])
   
-  ## Se invece è inferiore del tempo rilevato (time):
-  ## - nel caso il paziente non abbia avuto l'evento al tempo rilevato, non 
-  ##   l'ha avuto neanche al tempo precedente, pertanto bisogna
-  ##   portare indietro 
-  ##   il tempo lasciando stare la variabile indicatrice
-  ## - nel caso il paziente abbia avuto l'evento al tempo t, si
-  ##   ipotizza che a  t-1 non l'avesse (ovvero che un evento si
-  ##   verifica nel giorno in cui è registrato),  
-  ##   per cui si tratta di porre a 0 la variabile indicatrice
-  ##   e di riportare indietro il conteggio dei giorni al
-  ##   cutoff
-  ## In sostanza in entrambi i casi dummy=0 e time=censoring_time
-
+  ## working dataset: repeat/rbind the dataset for the number of censoring
+  ## times and add those in a column
+  db <- do.call(rbind,
+                list(data.frame(time, status))[rep(1, length(censoring_time))])
+  db$censoring_time <- rep(censoring_time, each = length(time))
+  
+  ## Move to C, please
   subst <- function(t,s,c) {
     if (t <= c) {
       c(t, s)
@@ -55,20 +71,21 @@ censor_at <- function(time = NULL, status = NULL, censoring_time = NULL) {
       c(c, 0)
     }
   }
-  
-  df <- data.frame(time, status)
-  rval <- data.frame(t(apply(df, 1,
-                             function(x)
-                               subst(t=x[1], s=x[2], c=censoring_time)
-                             ))) 
-  names(rval) <- paste(c("time", "status"),
-                       paste("c", censoring_time ,sep=""),
-                       sep="_")
+  ## now rval is a dataframe with long style
+  rval <- data.frame(t(apply(db, 1, function(x) {
+                               subst(t = x[1], s = x[2], c = x[3])
+                             })))
+                             
+
+  ## normalize output: rval as a wide style data.frame
+  rval <- do.call(cbind, split(rval, db$censoring_time))
+
+  ## TODO: names from deparse(substitute()) or similar
+  censoring_suffixes <- rep(sprintf("c%s", round(sort(censoring_time))),
+                            each = 2)   # for time and status
+  names(rval) <- paste(c("time", "status"), censoring_suffixes, sep = "_")
 
   rval
     
 }
-
-
-
 
---
+++ R/fup_completeness.R
@@ -0,0 +1,66 @@
+#' Calculate C index of of follow up completenes
+#' 
+#' 
+#' Calculate C index of of follow up completenes.
+#' 
+#' 
+#' @param time Follow up (in days?)
+#' @param status event indicator
+#' @param cutoff (in days?)
+#' @param strata group
+#' @return A list with individual global C-index, strata C-indexes
+#' (optional), and individual C-indexes
+#' @references Clark T., Altman D., De Stavola B. (2002),
+#' Quantification of the completeness of follow-up. Lancet 2002;
+#' 359: 1309-10
+#' @examples
+#'
+#' time   <- c(180, 12, 240, 250 )
+#' status <- c(  0,  1,   0, 1   )
+#' group  <- c("A","A", "B", "B" )
+#' 
+#' ## example: 
+#' ## quantify fup completeness to 200 days (eg minimum potential
+#' ## follow up in a hypotethic prospective trial)
+#' 
+#' fup_completeness(time = time, status = status, cutoff = 200, strata = group)
+#' 
+#' @export
+fup_completeness <- function(time = NULL,
+                             status = NULL,
+                             cutoff = NULL,
+                             strata = NULL) {
+  ## input validation
+  if (! is.numeric(time))
+    stop("time is mandatory and must be numeric.")
+  if (! is.numeric(status))
+    stop("status is mandatory and must be numeric.")
+  if (! is.numeric(cutoff))
+    stop("censoring_time is mandatory and must be numeric.")
+  
+  ## censoring to cutoff ...
+  db <- censor_at(time = time,
+                  status = status,
+                  censoring_time = cutoff)
+  names(db) <- c("time","status")
+  db$potential_fup <- ifelse(db$status == 0, cutoff, db$time)
+
+  ## global and individual C
+  res <- list("globalC" = sum(db$time) / sum(db$potential_fup),
+              "individualC" = db$time / db$potential_fup )
+
+  ## strata C
+  if (!is.null(strata)){
+    agg <- aggregate(db[c("time","potential_fup")],
+                     by = list(strata),
+                     FUN = sum )
+    agg$strataC <- agg[,2]/agg[,3]
+    res$strataC <-  agg
+  }
+
+  return(res)
+}
+
+
+
+
