7c33add16abfa955300c112c363c15579935ca39
Luca Braglia <lbraglia@gmail.com>
2016-07-05 14:53:40 +0200

    tests fixed

--- tests/testthat/test-fup_completeness.R
+++ tests/testthat/test-fup_completeness.R
@@ -9,13 +9,13 @@ test_that("fup completeness basic usage", {
                             status = status,
                             cutoff = 200,
                             strata = group)
-    right <- list('globalC' = 0.967320261437909,
-                  'individualC' = c(0.9, 1, 1, 1),
-                  'strataC' = data.frame('Group.1'       = c("A", "B"),
-                                         'time'          = c(192, 400),
-                                         'potential_fup' = c(212, 400),
-                                         'strataC' = c(0.905660377358491, 1),
-                                         stringsAsFactors = FALSE))
+    right <- structure(list(time = 200,
+                            overall = 0.967320261437909,
+                            A = 0.905660377358491,
+                            B = 1),
+                       .Names = c("time", "overall", "A", "B"),
+                       row.names = 1L, class = "data.frame")
+    
     expect_equal(res, right)
     
 })
