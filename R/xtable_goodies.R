5cd6667ed910e0bcc472917b8f9a4554641b6f98
Luca Braglia <lbraglia@gmail.com>
2016-07-27 11:21:02 +0200

    misc fixes to xtable.summary.coxph

--- R/xtable_goodies.R
+++ R/xtable_goodies.R
@@ -6,22 +6,22 @@
 #' @param print.coef print beta?
 #' @param print.se.coef print se(beta)?
 #' @param print.z print z?
+#' @param print.exp.coef print HR
+#' @param print.exp.coef.lci print HR lower confint
+#' @param print.exp.coef.uci print HR upper confint
 #' @param print.p print p?
-#' @param print.exp.coef print exp(beta)
-#' @param print.exp.coef.lci print confint(beta) low
-#' @param print.exp.coef.uci print confint(beta) low
 #' @param ... other things passed to \code{xtable::xtable}
 #' 
 #' @export
 xtable.summary.coxph <- function(x,
                                  digits = getOption("digits"),
-                                 print.coef = TRUE,
+                                 print.coef = FALSE,
                                  print.se.coef = FALSE,
                                  print.z = FALSE,
-                                 print.p = TRUE,
                                  print.exp.coef = TRUE,
                                  print.exp.coef.lci = TRUE,
                                  print.exp.coef.uci = TRUE,
+                                 print.p = TRUE,
                                  ...)                         
 {
 
@@ -30,19 +30,26 @@ xtable.summary.coxph <- function(x,
 
     coeffs <- x$coefficients
     ci <- x$conf.int
-    results <- cbind(coeffs, ci)
+    results <- as.data.frame(cbind(coeffs, ci))
 
-    column_select <- c(print.coef,
-                       FALSE,             #exp(coef)
-                       print.se.coef,
-                       print.z,
-                       print.p,
-                       print.exp.coef,
-                       FALSE,             #exp(-coef)
-                       print.exp.coef.lci,
-                       print.exp.coef.uci)
+    column_select <- c('coef',
+                       'se(coef)',
+                       'z',
+                       'exp(coef)',
+                       'lower .95',
+                       'upper .95',
+                       'Pr(>|z|)')[
+        c(print.coef,
+          print.se.coef,
+          print.z,
+          print.exp.coef,
+          print.exp.coef.lci,
+          print.exp.coef.uci,
+          print.p)
+    ]
   
     final_result <- results[ , column_select]
+    names(final_result)[names(final_result) %in% 'exp(coef)'] <- 'HR'
 
     xtable::xtable(final_result, digits = digits, ...)
 }
