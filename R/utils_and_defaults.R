d37e0a713a40794e9ca0b944eb9eeba42532994c
Luca Braglia <lbraglia@gmail.com>
2021-09-15 10:57:16 +0200

    update

--- R/utils_and_defaults.R
+++ R/utils_and_defaults.R
@@ -1,11 +1,13 @@
 time_unit_to_time_divisor <-
     function(time_unit = c("days", "weeks", "months", "years")){ 
         time_unit <- match.arg(time_unit)
+        mean_month = (365*3 + 366)/(12*4)
+        mean_year  = (365*3 + 366)/(4) 
         switch(time_unit,
                days = 1,
                weeks = 7,
-               months = 30.43,
-               years = 365.25)
+               months = mean_month,
+               years = mean_year)
 }
 
 default_time_by <- function(time_unit = c("days", "weeks", "months", "years")){
